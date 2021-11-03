b6dc964a4297b91b97b8414d32d89408ca34f25f
Johnsensei <john@johnsensei.com>
2021-11-03 10:37:52 -0500

    Adds 20211102 warmup 1 files for JavaScript and C#

---
+++ algorithm-problem-solving/20211102-warnmup-1.js
@@ -0,0 +1,23 @@
+let names = ['Geneviève', 'Odilon', 'Edouard', 'Mélaine', 'Raymond', 'Lucien', 'Alix', 'Guillaume', 'Paulin', 'Tatiana', 'Yvette', 'Nina', 'Rémi', 'Marcel', 'Roseline', 'Prisca', 'Marius', 'Sébastien', 'Agnès', 'Vincent', 'Barnard', 'François de Sales', 'Ananie', 'Paule', 'Angèle', "Thomas d'Aquin", 'Gildas', 'Martine', 'Marcelle', 'Février', 'Ella', 'Théophane', 'Blaise', 'Véronique', 'Agathe', 'Gaston', 'Eugénie', 'Jacqueline', 'Apolline', 'Arnaud', 'Héloïse', 'Félix', 'Béatrice', 'Valentin', 'Claude', 'Julienne', 'Alexis', 'Bernadette', 'Gabin', 'Aimée', 'Pierre-Damien', 'Isabelle', 'Lazare', 'Modeste', 'Roméo', 'Nestor', 'Honorine', 'Romain', 'Aubin', 'Charles le Bon', 'Guénolé', 'Casimir', 'Olive', 'Colette', 'Félicité', 'Jean de Dieu', 'Françoise', 'Vivien', 'Rosine', 'Justine', 'Rodrigue', 'Mathilde', 'Louise', 'Bénédicte', 'Patrice', 'Cyrille', 'Joseph', 'Herbert', 'Clémence', 'Léa', 'Victorien', 'Catherine de Suède', 'Humbert', 'Larissa', 'Habib', 'Gontran', 'Gwladys', 'Amédée', 'Benjamin', 'Hugues', 'Sandrine', 'Richard', 'Isidore', 'Irène', 'Marcellin', 'Jean-Baptiste de la Salle', 'Julie', 'Gauthier', 'Fulbert', 'Stanislas', 'Jules', 'Ida', 'Maxime', 'Paterne', 'Benoît-Joseph', 'Anicet', 'Parfait', 'Emma', 'Odette', 'Anselme', 'Alexandre', 'Georges', 'Fidèle', 'Marc', 'Alida', 'Zita', 'Valérie', 'Catherine de Sienne', 'Robert', 'Jérémie', 'Boris', 'Philippe, Jacques le Mineur', 'Sylvain', 'Judith', 'Prudence', 'Gisèle', 'Désiré', 'Pacôme', 'Solange', 'Estelle', 'Achille', 'Rolande', 'Matthias', 'Denise', 'Honoré', 'Pascal', 'Eric', 'Yves', 'Bernardin', 'Constantin', 'Emile', 'Didier', 'Donatien', 'Sophie', 'Bérenger', 'Augustin', 'Germain', 'Aymard', 'Ferdinand', 'Perrine', 'Justin', 'Blandine', 'Kévin', 'Clotilde', 'Igor', 'Norbert', 'Gilbert', 'Médard', 'Diane', 'Landry', 'Barnabé', 'Guy', 'Antoine de Padoue', 'Elisée', 'Germaine', 'Jean-François Régis', 'Hervé', 'Léonce', 'Romuald', 'Silvère', 'Rodolphe', 'Alban', 'Audrey', 'Jean-Baptiste', 'Prosper', 'Anthelme', 'Fernand', 'Irénée', 'Pierre/Paul', 'Martial', 'Thierry', 'Martinien', 'Thomas', 'Florent', 'Antoine', 'Mariette', 'Raoul', 'Thibaut', 'Amandine', 'Ulrich', 'Benoît', 'Olivier', 'Henri/Joël', 'Camille', 'Donald', 'Carmen', 'Charlotte', 'Frédéric', 'Arsène', 'Marina', 'Marie-Madeleine', 'Brigitte', 'Christine', 'Jacques', 'Anne/Joachim', 'Nathalie', 'Samson', 'Marthe', 'Juliette', 'Ignace de Loyola', 'Alphonse', 'Julien', 'Lydie', 'Jean-Marie', 'Abel', 'Octavien', 'Gaétan', 'Dominique', 'Amour', 'Laurent', 'Claire', 'Clarisse', 'Hippolyte', 'Evrard', 'Marie', 'Armel', 'Hyacinthe', 'Hélène', 'Jean-Eudes', 'Bernard', 'Christophe', 'Fabrice', 'Rose', 'Barthélémy', 'Louis', 'Natacha', 'Monique', 'Augustin', 'Sabine', 'Fiacre', 'Aristide', 'Gilles', 'Ingrid', 'Grégoire', 'Rosalie', 'Raissa', 'Bertrand', 'Reine', 'Adrien', 'Alain', 'Inès', 'Adelphe', 'Apollinaire', 'Aimé', 'Cyprien', 'Roland', 'Edith', 'Renaud', 'Nadège', 'Émilie', 'Davy', 'Matthieu', 'Maurice', 'Constant', 'Thècle', 'Hermann', 'Côme/Damien', 'Vincent de Paul', 'Venceslas', 'Michel', 'Jérôme', 'Thérèse', 'Léger', 'Gérard', 'François', 'Fleur', 'Bruno', 'Serge', 'Pélagie', 'Denis', 'Ghislain', 'Firmin', 'Wilfried', 'Géraud', 'Juste', 'Aurélie', 'Edwige', 'Baudouin', 'Luc', 'René', 'Adeline', 'Céline', 'Elodie', 'Jean de Capistran', 'Florentin', 'Crépin', 'Dimitri', 'Emeline', 'Simon', 'Narcisse', 'Bienvenue', 'Quentin', 'Harold', 'Océane', 'Hubert', 'Charles', 'Sylvie', 'Bertille', 'Carine', 'Geoffroy', 'Théodore', 'Léon', 'Martin', 'Christian', 'Brice', 'Sidoine', 'Albert', 'Marguerite', 'Elisabeth', 'Aude', 'Tanguy', 'Edmond', 'Rufus', 'Cécile', 'Clément', 'Flora', 'Catherine', 'Delphine', 'Séverin', 'Jacques de la Marche', 'Saturnin', 'André', 'Florence', 'Viviane', 'Xavier', 'Barbara', 'Gérald', 'Nicolas', 'Ambroise', 'Elfried', 'Pierre Fourier', 'Romaric', 'Daniel', 'Corentin', 'Lucie', 'Odile', 'Ninon', 'Alice', 'Gael', 'Gatien', 'Urbain', 'Théophile', 'Pierre Canisius', 'Françoise-Xavière', 'Armand', 'Adèle', 'Emmanuel', 'Étienne', 'Jean', 'Gaspard', 'David', 'Roger', 'Sylvestre']
+
+let sortedNames = names.sort();
+//console.log(sortedNames);
+
+function zemour (name) {
+  
+  for (let i = 0; i < sortedNames.length; i++) {
+
+    if (sortedNames[i] >= name) {
+      return sortedNames[i];
+    }
+  }
+
+}
+
+console.log(zemour("Alan")); // -> "Alban"
+console.log(zemour("Simon")); // -> "Simon"
+console.log(zemour("Malwenn")); // -> "Marc"
+console.log(zemour("John")); // -> "Joseph"
+console.log(zemour("Filomena")); // ->  "Firmin"
+console.log(zemour("Hertha")); // -> Hervé
+console.log(zemour("Hilda")); // -> Hippolyte
